8a624abc87658ef6f14cfa9881959ccab0635bc4
Omar <omarspedalieri.vfx@gmail.com>
2020-12-02 18:36:00 +0100

    setup

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+
+  el: " ",
+  data:{
+
+      },
+  mounted:function() {
+    // for (var i = 0; i < this.list_num; i++) {
+    //   axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    //   .then( mail => {
+    //     let item = mail.data.response;
+    //     this.mail_list.includes(item) ? i-- : this.mail_list.push(item);
+    //   });
+    }
+  }
+});
