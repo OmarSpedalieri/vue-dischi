0e5bc0bc5fda1c93bab4d7d0f0f6b0ee11ff648c
Omar <omarspedalieri.vfx@gmail.com>
2020-12-03 09:02:00 +0100

    bonus setup

--- js/script.js
+++ js/script.js
@@ -6,24 +6,50 @@
 // Concentratevi sulla parte JS,
 // per la grafica avete come ref lo screeshot,
 // ma i dettagli lasciateli per la fine.
+//
+// Creare una select con i seguenti generi: pop, rock, metal e jazz.
+// In base a cosa scegliamo nella select vedremo solo i corrispondenti cd.
+
 const collection = "https://flynn.boolean.careers/exercises/api/array/music";
 
 var app = new Vue({
 
   el: "#discografia",
   data:{
-    albums:[] // array vuoto che riempio interrogando l api con axios
+    albums:[], // array vuoto che riempio interrogando l api con axios
+    type:"all",
+    albums_picked:[]
   },
   mounted:function () {// mounted perchè viene generato all'inizio
 
     axios.get(collection).then(disc => {
+      //popolo il mio array album con le response di
+      // ogni elemento disc che mi manda axios
      this.albums = disc.data.response;
      // console.log(disc);
 
    })
+  },
+  methods:{
+    genrePicker(){
+      if (this.type === "all") {
+        this.albums_picked = this.albums;
+      }else{
+        this.albums_picked = this.albums.filter(item => item.genre === this.type);
+      }
+    }
+  },
+  computed:{
+    genreFilter : function(){
+      return this.albums.filter((album) => {
+
+       if (this.type === "all") {
+         return this.albums;
+       } else {
+         return album.genre === this.type;
+       }
+     })
+    }
   }
-
-
-
 
 });
